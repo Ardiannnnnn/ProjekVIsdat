821161fc2c0c58a82960a53afcd585b6da301bbc
rachmatfajar <rachmat3@mhs.unsyiah.ac.id>
2023-05-08 12:07:24 +0700

    update:revisi pie chart

--- js/piechart.js
+++ js/piechart.js
@@ -52,7 +52,7 @@ svg.selectAll("path")
   .append("path")
   .attr("d", arc)
   .attr("fill", function(d) { return color(d.data); })
-  .attr("stroke", "white")
+  .attr("stroke", "black")
   .style("stroke-width", "2px")
   .style("opacity", 0.9);
   
@@ -112,3 +112,174 @@ svg.append("text")
   .style("font-size", "16px")
   .style("text-decoration", "underline")
   .text("Legend");
+
+// make button to change the value of the pie chart
+d3.select("#button1").on("click", change);
+
+// change the value of the pie chart
+function change() {
+    const value = [125, 145];
+  const sum = value.reduce((a, b) => a + b, 0);
+  const dataPercentage = value.map((d) => (d / sum) * 100);
+
+  const arcs = pie(value);
+  
+  // set sum data name to the legend
+    const dataName = ["Female Having Lung Cancer", "Male Having Lung Cancer"];
+  // change legend value
+    svg.selectAll(".legend")
+    .data(dataName)
+    .transition()
+    .duration(1000)
+    .attr("transform", function(d, i) { return "translate(200," + (i * 20 - 100) + ")"; });
+
+    // change legend color
+    svg.selectAll(".legend rect")
+    .data(dataName)
+    .transition()
+    .duration(1000)
+    .style("fill", function(d, i) { return color(i); });
+
+    // change legend text
+    svg.selectAll(".legend text")
+    .data(dataName)
+    .transition()
+    .duration(1000)
+    .text(function(d) { return d; });
+
+  svg.selectAll("path")
+    .data(arcs)
+    .transition()
+    .duration(1000)
+    .attr("d", arc)
+    .attr("fill", function(d) { return color(d.data); })
+    .attr("stroke", "white")
+    .style("stroke-width", "2px")
+    .style("opacity", 0.7);
+
+  svg.selectAll("text")
+    .data(arcs)
+    .transition()
+    .duration(1000)
+    .text(function(d, i) { return dataPercentage[i].toFixed(1) + "%"; })
+    .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+    .style("text-anchor", "middle")
+    .style("font-size", 12)
+    .style("fill", "white");
+}
+
+// make button to change the value of the pie chart
+d3.select("#button2").on("click", change1);
+
+// change the value of the pie chart
+function change1() {
+    const value = [799, 1378, 1622, 3539, 4965, 5759, 6115, 9945, 10903, 10982];
+    const sum = value.reduce((a, b) => a + b, 0);
+    const dataPercentage = value.map((d) => (d / sum) * 100);
+
+    const arcs = pie(value);
+
+    // set sum data name to the legend
+        const dataName = ["Vatican City", "Tokelau", "Niue", "Falkland Islands", "Montserrat", "Saint Pierre Miquelon", "Saint Helena", "St. Barth", "Nauru", "Wallis and Futuna"];
+    // change legend value
+        svg.selectAll(".legend")
+        .data(dataName)
+        .transition()
+        .duration(1000)
+        .attr("transform", function(d, i) { return "translate(200," + (i * 20 - 100) + ")"; });
+
+        // change legend color
+        svg.selectAll(".legend rect")
+        .data(dataName)
+        .transition()
+        .duration(1000)
+        .style("fill", function(d, i) { return color(i); });
+
+        // change legend text
+        svg.selectAll(".legend text")
+        .data(dataName)
+        .transition()
+        .duration(1000)
+        .text(function(d) { return d; });
+
+    svg.selectAll("path")
+        .data(arcs)
+        .transition()
+        .duration(1000)
+        .attr("d", arc)
+        .attr("fill", function(d) { return color(d.data); })
+        .attr("stroke", "black")
+        .style("stroke-width", "2px")
+        .style("opacity", 0.9);
+
+    svg.selectAll("text")
+        .data(arcs)
+        .transition()
+        .duration(1000)
+        .text(function(d, i) { return dataPercentage[i].toFixed(1) + "%"; })
+        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+        .style("text-anchor", "middle")
+        .style("font-size", 12)
+        .style("fill", "white");
+}
+
+// make button to go back to the previous data of the pie chart
+d3.select("#button").on("click", change2);
+
+// go back to the previous data of the pie chart
+function change2() {
+    // change value to the previous data
+        const value = [7850, 8632, 17762, 18901, 19690, 20508, 20509, 23693, 24976, 25400];
+        const sum = value.reduce((a, b) => a + b, 0);
+        const dataPercentage = value.map((d) => (d / sum) * 100);
+
+    
+    const arcs = pie(value);
+    
+    // set sum data name to the legend
+        const dataName = ["Macao", "Falkland Islands", "Montserrat", "Antigua and Barbuda", "Cook Islands", "Wallis and Futuna", "Nauru", "Eritrea", "Vanuatu", "Saint Pierre Miquelon"];
+    // change legend value
+        svg.selectAll(".legend")
+        .data(dataName)
+        .transition()
+        .duration(1000)
+        .attr("transform", function(d, i) { return "translate(200," + (i * 20 - 100) + ")"; });
+    
+        // change legend color
+        svg.selectAll(".legend rect")
+        .data(dataName)
+        .transition()
+        .duration(1000)
+        .style("fill", function(d, i) { return color(i); });
+    
+        // change legend text
+        svg.selectAll(".legend text")
+        .data(dataName)
+        .transition()
+        .duration(1000)
+        .text(function(d) { return d; });
+    
+    svg.selectAll("path")
+        .data(arcs)
+        .transition()
+        .duration(1000)
+        .attr("d", arc)
+        .attr("fill", function(d) { return color(d.data); })
+        .attr("stroke", "black")
+        .style("stroke-width", "2px")
+        .style("opacity", 0.9);
+    
+    svg.selectAll("text")
+        .data(arcs)
+        .transition()
+        .duration(1000)
+        .text(function(d, i) { return dataPercentage[i].toFixed(1) + "%"; })
+        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+        .style("text-anchor", "middle")
+        .style("font-size", 12)
+        .style("fill", "white");
+    }
+
+
+
+
